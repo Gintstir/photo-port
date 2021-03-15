d5cbbf056f3e2b1272ddd6969f102e45586805da
Gint Stirbys <70384913+Gintstir@users.noreply.github.com>
2021-03-15 14:57:54 -0400

    tests written and passed for Nav

---
+++ src/components/Nav/__test__/index.test.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { render, cleanup, getByLabelText } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import Nav from '..';
+
+// 1) add the cleanup statement
+
+afterEach(cleanup);
+
+// 2) add the describe function to declare what this test suite will be testing:
+
+describe('Nav component', () => {
+    //baseline test
+    test('renders', () => {
+        render(<Nav />);
+    });
+    //snapshot test
+    test('matches snapshot', () => {
+        const { asFragment } = render(<Nav />);
+        //asert value comparison
+        expect(asFragment()).toMatchSnapshot();
+    })
+})
+
+describe('emoji is visible', () => {
+    test('inserts camera emoji into h2 element', () => {
+        //Arrange
+        const { getByLabelText } = render(<Nav />); 
+        //Assert
+        expect(getByLabelText('camera')).toHaveTextContent('📸')
+    })
+})
+
+describe('links are visible', () => {
+    it('inserts text into the links', () => {
+        //arrange
+        const { getByTestId } = render(<Nav />);
+        
+        //assert
+        expect(getByTestId('link')).toHaveTextContent('Oh Snap!');
+        expect(getByTestId('about')).toHaveTextContent('About Me');
+    });
+})
